1c58445349c3bcd475e8cb3636822bcac25fac30
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-10-29 20:39:13 +0800

    Pass week1-007.Multiplication exercise

---
+++ part-1/week1-007.Multiplication-5058704/src/Multiplication.java
@@ -0,0 +1,13 @@
+public class Multiplication {
+
+    public static void main(String[] args) {
+
+        int a = 1337;
+        int b = 42;
+        int product = a * b;
+        String toPrint = a + " * " + b + " = " + product;
+        // Program your solution here. Remember to use variables a and b!
+        System.out.println(toPrint);
+    }
+
+}
