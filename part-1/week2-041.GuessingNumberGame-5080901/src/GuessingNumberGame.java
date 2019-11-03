8c2958a6db6a31e9c3dfbad63b8ac1d8655af481
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-11-03 19:49:40 +0800

    Pass week1-041.GuessingNumberGame exercise

---
+++ part-1/week2-041.GuessingNumberGame-5080901/src/GuessingNumberGame.java
@@ -0,0 +1,33 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingNumberGame {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int numberDrawn = drawNumber();
+
+        // program your solution here. Do not touch the above lines!
+        int guessCount = 0;
+        
+        while (true) {
+            System.out.print("Guess a number: ");
+            int guess = Integer.parseInt(reader.nextLine());
+            guessCount++;
+            if (guess < numberDrawn) {
+                System.out.println("The number is greater, guesses made: " + guessCount);
+            } else if (guess > numberDrawn) {
+                System.out.println("The number is lesser, guesses made: " + guessCount);
+            } else {
+                System.out.println("Congratulations, your guess is correct!");
+                break;
+            }
+        }
+    }
+
+    // DO NOT MODIFY THIS!
+    private static int drawNumber() {
+        return new Random().nextInt(101);
+    }
+}
