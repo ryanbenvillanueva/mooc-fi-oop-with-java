59d6335f78376a4bd67c101f6b1f38bf5e209eb9
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-11-06 13:07:49 +0800

    Pass week4-072.1.YourFirstAccount exercise

---
+++ part-1/week4-072.1.YourFirstAccount-5097284/src/Accounts.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Accounts {
+
+    public static void main(String[] args) {
+        // Code in Account.Java should not be touched!
+        // write your code here
+        Account bensAccount = new Account("Ben's account", 100.00);
+        bensAccount.deposit(20.00);
+        System.out.println(bensAccount);
+    }
+
+}
