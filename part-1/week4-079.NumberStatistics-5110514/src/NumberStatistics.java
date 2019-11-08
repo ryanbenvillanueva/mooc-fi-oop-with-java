bbefefb94ac49e7b0346e4b90c43ffcd590633c6
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-11-08 14:05:27 +0800

    Pass week4-079.NumberStatistics exercise

---
+++ part-1/week4-079.NumberStatistics-5110514/src/NumberStatistics.java
@@ -0,0 +1,35 @@
+
+public class NumberStatistics {
+    private int amountOfNumbers;
+    private int sum;
+    
+    public NumberStatistics() {
+        this.amountOfNumbers = 0;
+        this.sum = 0;
+    }
+    
+    public void addNumber(int number) {
+        this.amountOfNumbers++;
+        sum += number;
+    }
+    
+    public int amountOfNumbers() {
+        return amountOfNumbers;
+    }
+    
+    public int sum() {
+        if (this.amountOfNumbers == 0) {
+            return 0;
+        } else {
+             return sum;
+        }
+    }
+    
+    public double average() {
+        if (this.amountOfNumbers == 0) {
+            return 0;
+        } else {
+            return (double) sum / this.amountOfNumbers;
+        }
+    }
+}
