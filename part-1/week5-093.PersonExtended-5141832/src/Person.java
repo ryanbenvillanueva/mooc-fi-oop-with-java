5620f6159070af5cad947e2500eaa9d295d1b17b
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-11-14 10:09:17 +0800

    Pass week5-093.PersonExtended exercise

---
+++ part-1/week5-093.PersonExtended-5141832/src/Person.java
@@ -0,0 +1,51 @@
+import java.util.Calendar;
+
+public class Person {
+    private String name;
+    private MyDate birthday;
+    
+    public Person(String name, int pp, int kk, int vv) {
+        this.name = name;
+        this.birthday = new MyDate(pp, kk, vv);
+    }
+    
+    public Person(String name, MyDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+    
+    public Person(String name) {
+        this.name = name;
+        this.birthday = getCurrentDate();
+    }
+    
+    public MyDate getCurrentDate() {
+        int day = Calendar.getInstance().get(Calendar.DATE);
+        int month = Calendar.getInstance().get(Calendar.MONTH) + 1; // January is 0 so we add one
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        
+        MyDate currentDate = new MyDate(day, month, year);
+        
+        return currentDate;
+    }
+    
+    public int age() {
+        // calculate the age based on the birthday and the current day
+        MyDate currentDate = getCurrentDate();
+        
+        return birthday.differenceInYears(currentDate);
+    }
+    
+    public boolean olderThan(Person compared) {
+        // compare the ages based on birthdays
+        return birthday.earlier(compared.birthday);
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public String toString() {
+        return this.name + ", born " + this.birthday;
+    }
+}
