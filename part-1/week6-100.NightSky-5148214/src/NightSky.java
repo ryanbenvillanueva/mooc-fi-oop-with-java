fd7bec798a128693eb69688399a14cb898aa7c55
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-11-15 16:46:29 +0800

    Pass week6-100.NightSky exercise

---
+++ part-1/week6-100.NightSky-5148214/src/NightSky.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ryan Ben
+ */
+import java.util.Random;
+
+public class NightSky {
+    private double density;
+    private int width;
+    private int height;
+    private int starsInLastPrint;
+    
+    public NightSky(double density) {
+        this(density, 20, 10);
+    }
+    
+    public NightSky(int width, int height) {
+        this(0.1, width, height);
+    }
+    
+    public NightSky(double density, int width, int height) {
+        this.density = density;
+        this.width = width;
+        this.height = height;
+    }
+    
+    public void printLine() {
+        Random random = new Random();
+        
+        for (int i = 0; i < width; i++) {
+            if (random.nextDouble() <= density) {
+                System.out.print("*");
+                starsInLastPrint++;
+            } else {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+    
+    public void print() {
+        starsInLastPrint = 0;
+        for(int i = 0; i < this.height; i++) {
+            printLine();
+        }
+    }
+    
+    public int starsInLastPrint(){
+        return starsInLastPrint;
+    }
+    
+}
