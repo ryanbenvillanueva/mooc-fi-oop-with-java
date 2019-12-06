d8e46c20387668ef14ddb5026592b370850ff736
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-12-07 05:49:55 +0800

    Pass week9-19.MethodArgumentvalidation exercise

---
+++ part-2/week9_19.MethodArgumentValidation-5245572/src/validation/Person.java
@@ -0,0 +1,25 @@
+package validation;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        if (name == null || name.isEmpty() || name.length() > 40 || (age < 0 || age > 120)) 
+        {
+            throw new IllegalArgumentException("Illegal argument");
+        }
+        
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
