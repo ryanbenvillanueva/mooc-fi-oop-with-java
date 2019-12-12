8646f578358e4859d2097bb439fbd6b01d8260e2
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-12-12 17:00:24 +0800

    Pass week9-24.MultipleEntryDictionary exercise

---
+++ part-2/week9_24.MultipleEntryDictionary-5268927/src/dictionary/PersonalMultipleEntryDictionary.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dictionary;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author Ryan Ben
+ */
+public class PersonalMultipleEntryDictionary implements MultipleEntryDictionary{
+    
+    private Map<String, Set<String>> dictionary;
+    
+    public PersonalMultipleEntryDictionary() {
+        this.dictionary = new HashMap<String, Set<String>>();
+    }
+    @Override
+    public void add(String word, String entry) {
+        if (!this.dictionary.containsKey(word)) {
+            this.dictionary.put(word, new HashSet<String>());
+        }
+        
+        this.dictionary.get(word).add(entry);
+    }
+
+    @Override
+    public Set<String> translate(String word) {
+        if (!this.dictionary.containsKey(word)) {
+            return null;
+        }
+        
+        return this.dictionary.get(word);
+    }
+
+    @Override
+    public void remove(String word) {
+        this.dictionary.remove(word);
+    }
+    
+}
